5293c1c719370ab99bad82193ad0383ef61ab8a7
mihirnimgade <mihir.nim@outlook.com>
2022-12-23 02:51:01 -0800

    Restructure tests

--- test/test_telemetry.py
+++ test/test_telemetry.py
@@ -1,126 +1,124 @@
-from link_telemetry import CANMessage
+from link_telemetry import StandardFrame
 from pathlib import Path
-import yaml
 import pprint
+import yaml
+import pytest
+import cantools
 
-YAML_FILE = Path("can.yaml")
+DBC_FILE = Path("./dbc/daybreak.dbc")
 
+# <---- helper functions ---->
 
-def test_motor_state():
-    # <----- setup ----->
+def check_equality(first: float, second: float, threshold=0.1):
+    return abs(first - second) <= threshold
 
-    # instantiate CANMessage
-    msg = CANMessage(b"0000A1FB05012F0F00FF00FF00FF7\n")
+# <---- test fixtures ---->
 
-    # read in the schema file
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
+@pytest.fixture
+def dbc():
+    # read in the DBC file
+    dbc = cantools.database.load_file(DBC_FILE)
+    return dbc
 
-        # <---- execution ---->
-
-        # extract messages
-        measurements = msg.extract_measurements(can_schema)
-
-        pprint.pprint(measurements)
-
-        assert measurements["bridge_pwm_flag"]["value"] == False
-        assert measurements["motor_current_flag"]["value"] == False
+# <---- tests ---->
 
 
-def test_speed_ctrl():
-    # <----- setup ----->
+class TestSpeedControllerMessages:
+    def test_speed_ctrl(self, dbc):
+        # instantiate StandardFrame
+        timestamp = b"0000A1FB"
+        id = b"0401"
+        payload = b"66662a42" + b"3333e341"
+        size = b"8"
 
-    # instantiate CANMessage
-    msg = CANMessage(b"0000A1FB040166662a423333e3417\n")
-
-    # read in the schema file
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        # <---- execution ---->
+        msg = StandardFrame(timestamp + id + payload + size + b"\n")
 
         # extract messages
-        measurements = msg.extract_measurements(can_schema)
-
-        print(measurements)
+        measurements = msg.extract_measurements(dbc)
 
         # checking 0x422a6666
-        assert measurements["desired_velocity"]["value"] == 42.6
+        assert check_equality(measurements["desired_velocity"]["value"], 42.6)
+
         # checking 0x41e33333
-        assert measurements["current_setpoint"]["value"] == 28.4
+        assert check_equality(measurements["current_setpoint"]["value"], 28.4)
 
 
-def test_motor_bus():
-    msg = CANMessage(b"0000A1FB0502cdcc52429a9999417\n")
+class TestMotorMessages:
+    def test_motor_state(self, dbc):
+        timestamp = b"0000A1FB"
+        id = b"0501"
+        payload = b"0223FAAB" + b"00000000"
+        size = b"7"
 
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
+        msg = StandardFrame(timestamp + id + payload + size + b"\n")
 
-        measurements = msg.extract_measurements(can_schema)
+        # extract messages
+        measurements = msg.extract_measurements(dbc)
 
-        print(measurements)
+        # byte 0
+        assert measurements["bridge_pwm_flag"]["value"] == False
+        assert measurements["motor_current_flag"]["value"] == True
+        assert measurements["velocity_flag"]["value"] == False
+        assert measurements["bus_current_flag"]["value"] == False
+        assert measurements["bus_voltage_upper_limit_flag"]["value"] == False
+        assert measurements["bus_voltage_lower_limit_flag"]["value"] == False
+        assert measurements["heat_sink_temp_flag"]["value"] == False
+
+        # byte 2
+        assert measurements["hw_overcurrent"]["value"] == False
+        assert measurements["sw_overcurrent"]["value"] == True
+        assert measurements["dc_bus_overvoltage"]["value"] == False
+        assert measurements["bad_hall_sequence"]["value"] == True
+        assert measurements["watchdog_reset"]["value"] == True
+        assert measurements["config_read_error"]["value"] == True
+        assert measurements["undervoltage_lockout"]["value"] == True
+
+    def test_motor_bus(self, dbc):
+        msg = StandardFrame(b"0000A1FB0502cdcc52429a9999417\n")
+
+        measurements = msg.extract_measurements(dbc)
 
         # check 0x4252cccd
-        assert measurements["bus_voltage"]["value"] == 52.7
+        assert check_equality(measurements["bus_voltage"]["value"], 52.7)
         # check 0x4199999a
-        assert measurements["bus_current"]["value"] == 19.2
+        assert check_equality(measurements["bus_current"]["value"], 19.2)
 
+    def test_motor_vel(self, dbc):
+        msg = StandardFrame(b"0000A1FB05030000b441cdcc87427\n")
 
-def test_motor_vel():
-    msg = CANMessage(b"0000A1FB05030000b441cdcc87427\n")
-
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        measurements = msg.extract_measurements(can_schema)
-
-        print(measurements)
+        measurements = msg.extract_measurements(dbc)
 
         # check 0x41b40000
-        assert measurements["motor_velocity"]["value"] == 22.5
+        assert check_equality(measurements["motor_velocity"]["value"], 22.5)
         # check 0x4287cccd
-        assert measurements["vehicle_velocity"]["value"] == 67.9
+        assert check_equality(measurements["vehicle_velocity"]["value"], 67.9)
 
+    def test_motor_phase_current(self, dbc):
+        msg = StandardFrame(b"0000A1FB05049a99b24200003e427\n")
 
-def test_motor_phase_current():
-    msg = CANMessage(b"0000A1FB05049a99b24200003e427\n")
-
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        measurements = msg.extract_measurements(can_schema)
-
-        print(measurements)
+        measurements = msg.extract_measurements(dbc)
 
         # check 0x42b2999a
-        assert measurements["phase_b_current"]["value"] == 89.3
+        assert check_equality(measurements["phase_b_current"]["value"], 89.3)
         # check 0x423e0000
-        assert measurements["phase_a_current"]["value"] == 47.5
+        assert check_equality(measurements["phase_a_current"]["value"], 47.5)
 
+    def test_motor_temp(self, dbc):
+        msg = StandardFrame(b"0000A1FB050B6766b242c3759c428\n")
 
-def test_motor_temp():
-    msg = CANMessage(b"0000A1FB050B6766b242c3759c428\n")
-
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        measurements = msg.extract_measurements(can_schema)
-
-        print(measurements)
+        measurements = msg.extract_measurements(dbc)
 
         # check 0x42b26667
-        assert measurements["motor_temperature"]["value"] == 89.2
+        assert check_equality(measurements["motor_temperature"]["value"], 89.2)
         # check 0x429c75c3
-        assert measurements["heatsink_temperature"]["value"] == 78.23
+        assert check_equality(measurements["heatsink_temperature"]["value"], 78.23)
 
 
-def test_battery_state():
-    msg = CANMessage(b"0000A1FB06229800196D45920C007\n")
+class TestBatteryMessages:
+    def test_battery_state(self, dbc):
+        msg = StandardFrame(b"0000A1FB06229800196D45920C007\n")
 
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        measurements = msg.extract_measurements(can_schema)
+        measurements = msg.extract_measurements(dbc)
 
         assert measurements["fault_state"]["value"] == True
         assert measurements["contactor_k1"]["value"] == False
@@ -149,15 +147,10 @@ def test_battery_state():
         assert measurements["low_soh"]["value"] == False
         assert measurements["isolation_fault"]["value"] == False
 
+    def test_battery_voltages(self, dbc):
+        msg = StandardFrame(b"0000A1FB0623001919F1A66215917\n")
 
-def test_battery_voltages():
-
-    msg = CANMessage(b"0000A1FB0623001919F1A66215917\n")
-
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        measurements = msg.extract_measurements(can_schema)
+        measurements = msg.extract_measurements(dbc)
 
         assert measurements["pack_voltage"]["value"] == 25
         assert measurements["min_voltage"]["value"] == 2.5
@@ -165,41 +158,26 @@ def test_battery_voltages():
         assert measurements["max_voltage"]["value"] == 16.6
         assert measurements["max_voltage_idx"]["value"] == 98
 
+    def test_battery_currents(self, dbc):
+        msg = StandardFrame(b"0000A1FB0624FFE70015002C00FF7\n")
 
-def test_battery_currents():
-
-    msg = CANMessage(b"0000A1FB0624FFE70015002C00FF7\n")
-
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        measurements = msg.extract_measurements(can_schema)
+        measurements = msg.extract_measurements(dbc)
 
         assert measurements["current"]["value"] == -25
         assert measurements["charge_limit"]["value"] == 21
         assert measurements["discharge_limit"]["value"] == 44
 
+    def test_battery_metadata(self, dbc):
+        msg = StandardFrame(b"0000A1FB062645000000000000007\n")
 
-def test_battery_metadata():
-
-    msg = CANMessage(b"0000A1FB062645000000000000007\n")
-
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        measurements = msg.extract_measurements(can_schema)
+        measurements = msg.extract_measurements(dbc)
 
         assert measurements["state_of_charge"]["value"] == 69
 
+    def test_battery_temp(self, dbc):
+        msg = StandardFrame(b"0000A1FB06275500322D5A5F00FF7\n")
 
-def test_battery_temp():
-
-    msg = CANMessage(b"0000A1FB06275500322D5A5F00FF7\n")
-
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
-
-        measurements = msg.extract_measurements(can_schema)
+        measurements = msg.extract_measurements(dbc)
 
         assert measurements["temperature"]["value"] == 85
         assert measurements["min_temperature"]["value"] == 50
