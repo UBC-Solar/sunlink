62c13f6de9b4c4c13649b6a693d8849aa59f3470
mihirnimgade <mihir.nim@outlook.com>
2022-12-22 17:34:26 -0800

    Implement DBC-based CAN parser

--- link_telemetry.py
+++ link_telemetry.py
@@ -1,11 +1,9 @@
 import serial
-import sys
+import cantools
 import influxdb_client
 from influxdb_client.client.write_api import ASYNCHRONOUS
 from pathlib import Path
-import yaml
 import pprint
-import struct
 import random
 import time
 import argparse
@@ -19,7 +17,7 @@ from dotenv import dotenv_values
 
 CAR_NAME = "Daybreak"
 
-YAML_FILE = Path("can.yaml")
+DBC_FILE = Path("./dbc/daybreak.dbc")
 ENV_FILE = Path(".env")
 
 ENV_CONFIG = dotenv_values(ENV_FILE)
@@ -43,12 +41,12 @@ GRAFANA_URL_NAME = Path(GRAFANA_URL).name
 # <----- Class definitions ------>
 
 
-class CANMessage:
+class StandardFrame:
     EXPECTED_CAN_MSG_LENGTH = 30
 
     def __init__(self, raw_string: bytes):
-        assert len(raw_string) == CANMessage.EXPECTED_CAN_MSG_LENGTH, \
-            f"raw_string not expected length of {CANMessage.EXPECTED_CAN_MSG_LENGTH}"
+        assert len(raw_string) == StandardFrame.EXPECTED_CAN_MSG_LENGTH, \
+            f"raw_string not expected length of {StandardFrame.EXPECTED_CAN_MSG_LENGTH}"
 
         self.timestamp = int(raw_string[0:8].decode(), 16)        # 8 bytes
         self.identifier = int(raw_string[8:12].decode(), 16)      # 4 bytes
@@ -61,6 +59,9 @@ class CANMessage:
 
         # separated into bytes (each byte represented in decimal)
         self.data = list(map(lambda x: int(x, 16), data))
+
+        # use this to decode the message
+        self.data_bytes: bytes = bytearray(self.data)
 
         self.hex_data = list(map(lambda x: hex(x), self.data))
 
@@ -79,13 +80,14 @@ class CANMessage:
         repr_str += f"{self.timestamp=}\n"
         repr_str += f"{self.data_len=}\n"
         repr_str += f"{self.data=}\n"
+        repr_str += f"{self.data_bytes.hex()=}\n"
         repr_str += f"{self.hex_data=}\n"
         repr_str += f"{self.bytestream=}\n"
         repr_str += f"{self.bitstream=}\n"
 
         return repr_str
 
-    def extract_measurements(self, schema: dict):
+    def extract_measurements(self, dbc):
         """
         Extracts measurements from the CAN message depending on the entries in
         the `schema` dict. Returns a measurement dict with the key as the measurement name
@@ -94,57 +96,22 @@ class CANMessage:
         Raises exception if schema does not contain key entry that matches `self.identifier`.
         """
 
-        # retrieve schema data for CAN ID
-        schema_data = schema.get(self.hex_identifier)
+        # decode message using DBC file
+        measurements = dbc.decode_message(self.identifier, self.data_bytes)
 
-        if schema_data is None:
-            raise ValueError(
-                f"WARNING: Schema not found for id={self.hex_identifier}, make entry in {YAML_FILE}\n")
-
-        measurements = schema_data.get("measurements")
+        message = dbc.get_message_by_frame_id(self.identifier)
 
         # where the data came from
-        source = schema_data.get("source")
-
-        # the "name" of the CAN message
-        measurement_class = schema_data.get("name")
+        sources: list = message.senders
+        source: str = sources[0]
 
         measurement_dict = dict()
 
         for name, data in measurements.items():
-            bits = data["bits"]
-            measurement_type = data["type"]
-
             measurement_dict[name] = dict()
-
-            # extract measurement from CAN message bitstream
-
-            # if only a single bound is provided, extract single bit
-            if len(bits) == 1:
-                bit_index = bits[0]
-                extracted_value = self.bitstream[bit_index]
-
-            # if both bounds are provided, extract range of bits
-            if len(bits) == 2:
-                lower = bits[0]
-                upper = bits[1]
-
-                # lower and upper are both inclusive bounds
-                extracted_value = self.bitstream[lower:upper+1]
-
-            # convert binary bitstream values to integers
-            processing_fn = TYPE_PROCESSING_MAP.get(measurement_type)
-
-            if processing_fn is None:
-                raise ValueError(
-                    f"WARNING: no entry for {measurement_type} found in {TYPE_PROCESSING_MAP=}\n")
-
-            processed_value = processing_fn(extracted_value)
-
-            # place into measurement dictionary
+            measurement_dict[name]["class"] = message.name
             measurement_dict[name]["source"] = source
-            measurement_dict[name]["class"] = measurement_class
-            measurement_dict[name]["value"] = processed_value
+            measurement_dict[name]["value"] = data
 
         return measurement_dict
 
@@ -155,76 +122,18 @@ class CANMessage:
         for i in range(0, len(lst), n):
             yield lst[i: i+n]
 
-    @staticmethod
-    def twos_complement8(byte: str):
-        """
-        Interprets byte as two's complement signed integer.
-        NOTE: Byte is assumed to be big-endian (MSB first)
-        """
-
-        assert len(byte) == 8, "`byte` argument must be length 8"
-
-        sign = int(byte[0], 2)
-        tail = int(byte[1:], 2)
-
-        # if number is negative
-        if sign == 1:
-            # invert and add one
-            invert_tail = 127 - tail
-            value = invert_tail + 1
-            return -1 * value
-
-        return tail
-
-    @staticmethod
-    def twos_complement16(word: str):
-        """
-        Interprets word as two's complement signed integer.
-        NOTE: Word is assumed to be big-endian (MSB first)
-        """
-
-        assert len(word) == 16, "`word` argument must be length 16"
-
-        sign = int(word[0], 2)
-        tail = int(word[1:], 2)
-
-        # if number is negative
-        if sign == 1:
-            # invert and add one
-            invert_tail = 32767 - tail
-            value = invert_tail + 1
-            return -1 * value
-
-        return tail
-
-    @staticmethod
-    def ieee32_to_float(dword: str):
-        # dword must be a 32-bit binary number
-        assert len(dword) == 32, "dword is not length 32"
-
-        i = int(dword, 2)
-        return struct.unpack(">f", struct.pack("I", i))[0]
-
-
-TYPE_PROCESSING_MAP = {
-    "bool": lambda x: True if int(x, 2) == 1 else False,
-    "unsigned": lambda x: int(x, 2),
-    "signed_8": CANMessage.twos_complement8,
-    "signed_16": CANMessage.twos_complement16,
-    "incremental": lambda x: int(x, 2) * 0.1,
-    "ieee32_float": lambda x: round(CANMessage.ieee32_to_float(x), 2)
-}
-
-
-def random_can_str(can_schema) -> str:
-    can_ids = list(can_schema.keys())
+def random_can_str(dbc) -> str:
+    # collect CAN IDs
+    can_ids = list()
+    for message in dbc.messages:
+        can_ids.append(message.frame_id)
 
     # 0 to 2^32
     random_timestamp = random.randint(0, pow(2, 32))
     random_timestamp_str = "{0:0{1}x}".format(random_timestamp, 8)
 
     # random identifier
-    random_identifier = int(random.choice(can_ids)[2:], 16)
+    random_identifier = random.choice(can_ids)
     random_id_str = "{0:0{1}x}".format(random_identifier, 4)
 
     # random data
@@ -254,7 +163,8 @@ async def main():
     debug_group.add_argument("-d", "--debug", action="store_true", help=("Enables debug mode. This allows using the "
                                                                          "telemetry link with randomly generated CAN "
                                                                          "data rather using an actual radio telemetry stream"))
-    debug_group.add_argument("--no-write", action="store_true", help=("Disables writing to InfluxDB bucket and Grafana live stream endpoints"))
+    debug_group.add_argument("--no-write", action="store_true", help=(
+        "Disables writing to InfluxDB bucket and Grafana live stream endpoints"))
 
     normal_group.add_argument("-p", "--port", action="store",
                               help=("Specifies the serial port to read radio data from. "
@@ -278,17 +188,17 @@ async def main():
 
     # <----- InfluxDB object set-up ----->
 
-    client = influxdb_client.InfluxDBClient(url=INFLUX_URL, org=INFLUX_ORG, token=INFLUX_TOKEN)
+    client = influxdb_client.InfluxDBClient(
+        url=INFLUX_URL, org=INFLUX_ORG, token=INFLUX_TOKEN)
     write_api = client.write_api(write_options=ASYNCHRONOUS)
 
-    # <----- Read in YAML CAN schema file ----->
+    # <----- Read in DBC file ----->
 
-    with open(YAML_FILE, "r") as f:
-        can_schema: dict = yaml.safe_load(f)
+    daybreak_dbc = cantools.database.load_file('./dbc/daybreak.dbc')
 
     while True:
         if args.debug:
-            message = random_can_str(can_schema)
+            message = random_can_str(daybreak_dbc)
             message = message.encode(encoding="UTF-8")
             time.sleep(0.5)
         else:
@@ -301,19 +211,19 @@ async def main():
                 # read in bytes from COM port
                 message = ser.readline()
 
-                if len(message) != CANMessage.EXPECTED_CAN_MSG_LENGTH:
+                if len(message) != StandardFrame.EXPECTED_CAN_MSG_LENGTH:
                     print(
-                        f"WARNING: got message length {len(message)}, expected {CANMessage.EXPECTED_CAN_MSG_LENGTH}. Dropping message...")
+                        f"WARNING: got message length {len(message)}, expected {StandardFrame.EXPECTED_CAN_MSG_LENGTH}. Dropping message...")
                     print(message)
                     continue
 
-        can_msg = CANMessage(raw_string=message)
+        can_msg = StandardFrame(raw_string=message)
 
         # extract measurements from CAN message
         try:
-            extracted_measurements = can_msg.extract_measurements(can_schema)
+            extracted_measurements = can_msg.extract_measurements(daybreak_dbc)
             print(can_msg)
-            # pp.pprint(extracted_measurements)
+            pp.pprint(extracted_measurements)
         except ValueError as exc:
             print(exc)
             continue
