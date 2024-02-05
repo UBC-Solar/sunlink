fe429b1d38744d79cdfa736c578b88d21e6ee591
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-05 10:10:42 -0800

    m

--- parser/dbctest.py
+++ parser/dbctest.py
@@ -1,21 +1,43 @@
 import cantools
 from pathlib import Path
 import os
-from randomizer import RandomMessage
-from create_message import create_message
 
-dbc = cantools.database.load_file(str(Path(os.getcwd()) / "dbc" / "brightside.dbc"))
-msg = RandomMessage().random_can_str(dbc)
-print(msg)
-can = create_message(msg)
-print(can.extract_measurements(dbc))
 
-gps = RandomMessage().random_gps_str()
-print(gps)
-gps = create_message(gps)
-print(gps.extract_measurements())
+class Potato:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return "Potato"
+    
+class Cheese:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return "Cheese"
 
-imu = RandomMessage().random_imu_str()
-print(imu)
-imu = create_message(imu)
-print(imu.extract_measurements())
+def create(num):
+    if (num == 1):
+        return Potato("p")
+    else:
+        return Cheese("q")
+    
+
+string: str = type(create(0))
+print(string)
+
+
+# dbc = cantools.database.load_file(str(Path(os.getcwd()) / "dbc" / "brightside.dbc"))
+# msg = RandomMessage().random_can_str(dbc)
+# print(msg)
+# can = create_message(msg)
+# print(can.extract_measurements(dbc))
+
+# gps = RandomMessage().random_gps_str()
+# print(gps)
+# gps = create_message(gps)
+# print(gps.extract_measurements())
+
+# imu = RandomMessage().random_imu_str()
+# print(imu)
+# imu = create_message(imu)
+# print(imu.extract_measurements())
