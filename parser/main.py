ba483d5c9391526986f218489fbddbef9180a609
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-05 09:52:41 -0800

    m

--- parser/main.py
+++ parser/main.py
@@ -21,6 +21,7 @@ from flask_httpauth import HTTPTokenAuth
 from parser.standard_frame import StandardFrame
 from parser.standard_frame import Measurement
 
+from parser.Message import Message
 from parser.randomizer import RandomMessage
 
 # # New imports
