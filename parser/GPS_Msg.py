110e2981aa682f84414b036bfaf2c1e75506743f
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-10 13:06:40 -0800

    r

--- parser/GPS_Msg.py
+++ parser/GPS_Msg.py
@@ -3,25 +3,25 @@ import re
 """
 GPS message wrapper class. GPS.data[''] fields are:
 
-"Latitude": degrees + latside (N or S)
-"Longitude": degrees + longside (E or W)
-"Altitude": meters
-"HDOP": horizontal dilution of precision
-"Satellites": number of satellites
-"Fix": 0 or 1 -- 0 = no fix, 1 = fix
-"Time": seconds since last measurement
-"ID":  ID of GPS messages chosen to be the timestamp of the message (Time field)
+'latitude':   (double) degrees
+'latside':    (char) N or S
+'longitude':  (double) degrees
+'longside':   (char) E or W
+'altitude':   (double) meters
+'hdop':       (double) horizontal dilution of precision
+'satellites': (int) number of satellites
+'fix':        (int) 0 or 1 -- 0 = no fix, 1 = fix
+'timestamp':       (double) seconds since last measurement
 
 self.type = "GPS"
 """
 class GPS:
-    def __init__(self, message: str, format_specifier=None) -> None:   
+    def __init__(self, message: str) -> None:   
         # Parse all data fields and set type
-        self.message = message
-        self.data = self.extract_measurements()
+        self.data = self.parseGPS_str(message)
         self.type = "GPS"
 
-    
+
     """
     Extracts measurements from a GPS message based on a specified format
     Keys of the dict are column headings. Values are data (as strings) in column.
@@ -30,7 +30,7 @@ class GPS:
         format_specifier: None for GPS messages (as of now)
         
     Returns:
-        dictionary with the following form
+        display_data dictionary with the following form
         {
             "Latitude": [val + latside],
             "Longitude": [val + longside],
@@ -43,28 +43,50 @@ class GPS:
         }
     """
     def extract_measurements(self, format_specifier=None) -> dict:
+        display_data = {
+            "Latitude": [str(self.data['latitude']) + " " + self.data['latSide']],
+            "Longitude": [str(self.data['longitude']) + " " + self.data['lonSide']],
+            "Altitude": [str(self.data['altitude'])],
+            "HDOP": [str(self.data['hdop'])],
+            "Satellites": [str(self.data['satelliteCount'])],
+            "Fix": [str(self.data['fix'])],
+            "Time": [str(self.data['lastMeasure'])],
+            "ID": str(self.data['lastMeasure'])
+        }
+
+        return display_data
+
+    """
+    Parses a GPS message string into a dictionary of fields using regex
+    
+    Parameters:
+        str_msg: the GPS message as a string
+
+    Returns:
+        a dictionary containing the data fields of the GPS message
+    """
+    def parseGPS_str(self, str_msg: str) -> dict:
         pattern = (
-            r"Latitude: (?P<Latitude>-?\d+\.\d+) (?P<latSide>[NS]), "
-            r"Longitude: (?P<Longitude>-?\d+\.\d+) (?P<longSide>[EW]), "
-            r"Altitude: (?P<Altitude>-?\d+\.\d+) meters, "
-            r"HDOP: (?P<HDOP>-?\d+\.\d+), "
-            r"Satellites: (?P<Satellites>\d+), "
-            r"Fix: (?P<Fix>\d+), "
-            r"Time: (?P<Timestamp>\d+\.\d+)"
+            r"Latitude: (?P<latitude>-?\d+\.\d+) (?P<latSide>[NS]), "
+            r"Longitude: (?P<longitude>-?\d+\.\d+) (?P<lonSide>[EW]), "
+            r"Altitude: (?P<altitude>-?\d+\.\d+) meters, "
+            r"HDOP: (?P<hdop>-?\d+\.\d+), "
+            r"Satellites: (?P<satelliteCount>\d+), "
+            r"Fix: (?P<fix>\d+), "
+            r"Time: (?P<lastMeasure>\d+\.\d+)"
         )
-        match = re.search(pattern, self.message)
+        match = re.search(pattern, str_msg)
         
         if match:
             gps_data = match.groupdict()
-            gps_data['Latitude'] = [str(float(gps_data['Latitude'])) + " " + gps_data['latSide']] 
-            gps_data['Longitude'] = [str(float(gps_data['Longitude'])) + " " + gps_data['longSide']] 
-            gps_data['Altitude'] = [str(float(gps_data['Altitude']))]
-            gps_data['HDOP'] = [str(float(gps_data['HDOP']))]
-            gps_data['Satellites'] = [str(int(gps_data['Satellites']))]
-            gps_data['Fix'] = [str(int(gps_data['Fix']))]
-            gps_data['Timestamp'] = [str(float(gps_data['Timestamp']))]
+            gps_data['latitude'] = float(gps_data['latitude'])
+            gps_data['longitude'] = float(gps_data['longitude'])
+            gps_data['altitude'] = float(gps_data['altitude'])
+            gps_data['hdop'] = float(gps_data['hdop'])
+            gps_data['satelliteCount'] = int(gps_data['satelliteCount'])
+            gps_data['fix'] = int(gps_data['fix'])
+            gps_data['timestamp'] = float(gps_data['lastMeasure'])
             
-            print(gps_data)
             return gps_data
         else:
             return None
