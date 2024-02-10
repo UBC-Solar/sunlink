ace898d5c1672f7864b95d8c216b433d97560275
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-10 14:16:47 -0800

    CAN done

--- parser/CAN_Msg.py
+++ parser/CAN_Msg.py
@@ -2,15 +2,12 @@
 Subclass CAN implements the extract_measurements and getter
 methods from the interface Message. Data fields are:
 
-'timestamp':      the timestamp of the CAN message (All D's right now)
-'identifier':     the ID of the CAN message in decimal
-'data_len':       the number of valid bytes in the CAN message payload (0-8)
-'hex_identifier': the ID of the CAN message in hex (0x000 - 0x7ff)
-'data_list':      the payload of the CAN message as a list of ints
-'data_bytes':     the payload of the CAN message as a bytearray
-'hex_data':       the payload of the CAN message separated into bytes (hex)
-'bytestream':     the payload of the CAN message separated into bytes (binary)
-'bitstream':      the payload of the CAN message as a single binary number
+"Hex_ID": The ID of the CAN message in hex
+"Source": The board which the message came from
+"Class": The class of the message (Ex. Voltage Sensors Data)
+"Measurment": Specifc measurement name in this class (Ex. Volt Sensor 1, Volt Sensor 2)
+"Value": The value of the associated measurement
+"ID": Chosen to be the Hex_ID
 
 self.type = "CAN"
 """
@@ -34,7 +31,7 @@ class CAN:
     This is done to match list length to number of measurement in CAN message to list
     
     Parameters:
-        format_specifier: file path to a file containing a format specifier
+        format_specifier: DBC object from cantools
         
     Returns:
         display_data dictionary with the following form
@@ -48,44 +45,16 @@ class CAN:
         }
     """
     def extract_measurements(self, format_specifier=None) -> dict:      
-        timestamp: str = self.message[0:8]
+        timestamp: int = int(self.message[0:8], 16)
         id: str = self.message[8:12]
         data: str = self.message[12:20]
-        data_len: str = self.message[20:21]
 
-        self.timestamp = int(timestamp, 16)     # 8 bytes
-        self.identifier = int(id, 16)           # 4 bytes
-        self.data_len = int(data_len, 16)       # 1 byte
+        identifier = int(id, 16)  
+        data_bytes = bytearray(map(lambda x: ord(x), data))
+        hex_id = "0x" + hex(identifier)[2:].upper()
 
-        self.hex_identifier = "0x" + hex(self.identifier)[2:].upper()
-
-        self.data_list = list(map(lambda x: ord(x), data))            # 16 bytes
-        self.hex_data = list(map(lambda x: hex(x), self.data_list))
-        # separated into bytes (each byte represented in decimal)
-
-        self.data_bytes: bytes = bytearray(map(lambda x: ord(x), data))
-
-        # separated into bytes (each byte represented in binary)
-        self.bytestream = list(map(lambda x: "{0:08b}".format(ord(x)), data))
-
-        # single binary number representing the CAN message data
-        self.bitstream = "".join(self.bytestream)
-
-        # Create dictionary for data and set the type
-        self.data = {
-            "timestamp": self.timestamp,
-            "identifier": self.identifier,
-            "data_len": self.data_len,
-            "hex_identifier": self.hex_identifier,
-            "data_list": self.data_list,
-            "data_bytes": self.data_bytes,
-            "hex_data": self.hex_data,
-            "bytestream": self.bytestream,
-            "bitstream": self.bitstream
-        }
-
-        measurements = format_specifier.decode_message(self.data["identifier"], self.data["data_bytes"])
-        message = format_specifier.get_message_by_frame_id(self.data["identifier"])
+        measurements = format_specifier.decode_message(identifier, data_bytes)
+        message = format_specifier.get_message_by_frame_id(identifier)
 
         # where the data came from
         sources: list = message.senders
@@ -103,15 +72,17 @@ class CAN:
             "Class": [],
             "Measurement": [],
             "Value": [],
-            "ID": self.data["hex_identifier"]
+            "Timestamp": [],
+            "ID": hex_id
         }
 
         # Now add each field to the list
         for name, data in measurements.items():
-            display_data["Hex_ID"].append(self.data["hex_identifier"])
+            display_data["Hex_ID"].append(hex_id)
             display_data["Source"].append(source)
             display_data["Class"].append(message.name)
             display_data["Measurement"].append(name)
+            display_data["Timestamp"].append(timestamp)
             display_data["Value"].append(data)
         
         return display_data
