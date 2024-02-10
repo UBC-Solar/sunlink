cc6139a5a3da58236dc1e36cb2f630d03fece7a3
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-10 14:51:36 -0800

    Updted script for CAN, GPS, IMU _test buckets

--- scripts/create-influx_debug-bucket.sh
+++ scripts/create-influx_debug-bucket.sh
@@ -3,3 +3,12 @@ set -e
 
 # creates the debug bucket after InfluxDB initialization
 influx bucket create --name "${INFLUX_DEBUG_BUCKET}" --org "${DOCKER_INFLUXDB_INIT_ORG}"
+
+# Creates the CAN_test bucket
+influx bucket create --name "CAN_test" --org "${DOCKER_INFLUXDB_INIT_ORG}"
+
+# Creates the GPS_test bucket
+influx bucket create --name "GPS_test" --org "${DOCKER_INFLUXDB_INIT_ORG}"
+
+# Creates the IMU_test bucket
+influx bucket create --name "IMU_test" --org "${DOCKER_INFLUXDB_INIT_ORG}"
