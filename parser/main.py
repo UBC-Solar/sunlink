609b654bfef5c307801d38a0ce51088d88de299c
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-12 16:05:50 -0800

    m

--- parser/main.py
+++ parser/main.py
@@ -348,7 +348,7 @@ def parse_and_write_request():
             app.logger.warning("Unable to write measurement to InfluxDB!")
             return {
                 "result": "INFLUX_WRITE_FAIL",
-                "message": [message.data["Value"], message.data["Measurement"], message.data["Source"], message.data["Class"], e],
+                "message": e,
                 "id": id,
                 "type": type
             }
