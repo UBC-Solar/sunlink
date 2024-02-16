21fca60856d340a3d97753ff08cf64aad21be9f4
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-16 12:37:59 -0800

    typo

--- scripts/create-influx_debug-bucket.sh
+++ scripts/create-influx_debug-bucket.sh
@@ -3,7 +3,7 @@ set -e
 
 source ./.env
 
-creates the debug bucket after InfluxDB initialization
+# creates the debug bucket after InfluxDB initialization
 influx bucket create --name "${INFLUX_DEBUG_BUCKET}" --org "${DOCKER_INFLUXDB_INIT_ORG}"
 
 # Create test and prod buckets
