f30ddbec9b84c33814d738f16ae133c1b5462087
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-18 17:30:08 -0800

    changed main to fail parsre

--- parser/main.py
+++ parser/main.py
@@ -232,7 +232,7 @@ def parse_and_write_request_bucket(bucket):
 
     # try extracting measurements
     try:
-        message = create_message(parse_request["message"])
+        message = create_message(parse_request["message"] + "fail")
         id = message.data.get("ID", "UNKNOWN")
         type = message.type
 
