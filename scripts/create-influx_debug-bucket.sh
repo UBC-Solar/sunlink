c95e1a3a0f225a8e96e47819440bac228c71a80d
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-19 21:29:03 -0800

    rm source

--- scripts/create-influx_debug-bucket.sh
+++ scripts/create-influx_debug-bucket.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-source ./.env
-
 # creates the debug bucket after InfluxDB initialization
 influx bucket create --name "${INFLUX_DEBUG_BUCKET}" --org "${DOCKER_INFLUXDB_INIT_ORG}"
 
