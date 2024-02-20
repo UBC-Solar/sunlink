778d6ee297865a284b30f01f403f18ea6cc2135e
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-20 13:01:22 -0800

    attempting script

--- scripts/create-influx_debug-bucket.sh
+++ scripts/create-influx_debug-bucket.sh
@@ -3,3 +3,14 @@ set -e
 
 # creates the debug bucket after InfluxDB initialization
 influx bucket create --name "${INFLUX_DEBUG_BUCKET}" --org "${DOCKER_INFLUXDB_INIT_ORG}"
+
+# Create test and prod buckets
+IFS=',' read -ra TYPES <<< "${MESSAGE_TYPES}"
+
+for TYPE in "${TYPES[@]}"; do
+    # Create the debug bucket
+    influx bucket create --name "${TYPE}_test" --org "${DOCKER_INFLUXDB_INIT_ORG}"
+    
+    # Create the prod bucket
+    influx bucket create --name "${TYPE}_prod" --org "${DOCKER_INFLUXDB_INIT_ORG}"
+done
