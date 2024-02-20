f28161bacb3b35a8fa64d831d117754fdfada068
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-02-19 21:36:39 -0800

    reverting config changes

--- scripts/create-influx_debug-bucket.sh
+++ scripts/create-influx_debug-bucket.sh
@@ -3,16 +3,3 @@ set -e
 
 # creates the debug bucket after InfluxDB initialization
 influx bucket create --name "${INFLUX_DEBUG_BUCKET}" --org "${DOCKER_INFLUXDB_INIT_ORG}"
-
-# # Create test and prod buckets
-# IFS=',' read -ra TYPES <<< "${MESSAGE_TYPES}"
-
-# for TYPE in "${TYPES[@]}"; do
-#     # Create the debug bucket
-#     influx bucket create --name "${TYPE}${INFLUX_DEBUG_BUCKET}" --org "${DOCKER_INFLUXDB_INIT_ORG}"
-    
-#     # Create the prod bucket
-#     influx bucket create --name "${TYPE}${INFLUX_PROD_BUCKET}" --org "${DOCKER_INFLUXDB_INIT_ORG}"
-# done
-
-
