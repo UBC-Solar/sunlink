52a75421be6e87aff229091a06c3a65edcf147a1
AarjavJain101 <Aarjavjain2736@gmail.com>
2024-03-11 21:10:47 -0700

    trying to flush with .close()

--- parser/main.py
+++ parser/main.py
@@ -278,6 +278,7 @@ def parse_and_write_request_bucket(bucket):
         # write to InfluxDB
         try:
             write_api.write(bucket=message.type + bucket, org=INFLUX_ORG, record=point)
+            write_api.close()
         except Exception as e:
             app.logger.warning("Unable to write measurement to InfluxDB!")
             return {
