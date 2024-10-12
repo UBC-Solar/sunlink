f80d8b9c18c677ce6fb09a6b72123e64cf529f74
Joshua Riefman <joshuariefman@gmail.com>
2024-10-12 13:11:32 -0700

    Add target config

--- tools/PostgresUploader.py
+++ tools/PostgresUploader.py
@@ -1,49 +1,25 @@
-from sqlalchemy import create_engine, Column, Integer, Float, DateTime, UniqueConstraint
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
+import pathlib
+
 from parser.main import create_message
 from dotenv import load_dotenv
-import os
 import threading
 from data_tools import PostgresClient
+from data_tools.query.data_schema import CANLog, get_sensor_id
+import tomllib
+
 
 load_dotenv()
 
-POSTGRESQL_USERNAME = os.getenv("POSTGRESQL_USERNAME")
-POSTGRESQL_PASSWORD = os.getenv("POSTGRESQL_PASSWORD")
-POSTGRESQL_DATABASE = os.getenv("POSTGRESQL_DATABASE")
-POSTGRESQL_ADDRESS  = os.getenv("POSTGRESQL_ADDRESS")
 
-DATABASE_URL = f"postgresql://{POSTGRESQL_USERNAME}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_ADDRESS}:5432/{POSTGRESQL_DATABASE}"
-
-print(DATABASE_URL)
-
-engine = create_engine(DATABASE_URL)
-
-Base = declarative_base()
-
-class CANLog(Base):
-    __tablename__ = "sensor_data"
-
-    id = Column(Integer, primary_key=True)
-    timestamp = Column(Float, nullable=False)
-    sensor_type = Column(Integer, nullable=False)
-    value = Column(Float, nullable=False)
-
-    # Composite unique constraint
-    __table_args__ = (
-        UniqueConstraint('timestamp', 'sensor_type', name='unique_timestamp_sensor_type'),
-    )
+postgres_client = PostgresClient("can_log_prod")
 
 
-Base.metadata.create_all(engine)            # Only needs to be called once, but has no effect if it was already called
+thread_local_storage = threading.local()    # Per-thread local storage
 
 
-Session = sessionmaker(bind=engine)         # Session builder
-
-
-thread_local_storage = threading.local()    # Per-thread local storage 
+with open(str(pathlib.Path(__file__.parent.parent / "config" / "postgres.toml")), "rb") as f:
+    postgres_targets = tomllib.load(f)
+    targets = postgres_targets["config"]["target"]
 
 
 def get_session():
@@ -51,27 +27,13 @@ def get_session():
     Get the session for the current thread, creating it if necessary.
     """
     if not hasattr(thread_local_storage, 'session'):
-        thread_local_storage.session = Session()
+        thread_local_storage.session = postgres_client.get_session()
     return thread_local_storage.session
-
-
-targets = ["BatteryCurrent", "BatteryVoltage", "VoltageofLeast" ]
-
-
-sensor_encodings = {
-    "TotalPackVoltage": 0,
-    "BatteryCurrent": 1,
-    "BatteryVoltage": 2,
-    "VehicleVelocity": 3,
-    "PackCurrent": 4,
-    "BatteryCurrentDirection": 5,
-    "VoltageofLeast": 6
-}
 
 
 def parse_message(msg):
     """
-    Parses incoming request and sends back the parsed result.
+    Parse a message and
     """    
     local_session = get_session()  # Get the thread-local session
     msgs = []
@@ -89,7 +51,9 @@ def parse_message(msg):
 
             for i in range(len(message.data["Measurement"])):
                 if (measurement := message.data['Measurement'][i]) in targets:
-                    sensor_logs.append(CANLog(timestamp=message.data['Timestamp'][i], sensor_type=sensor_encodings[measurement], value=message.data['Value'][i]))
+                    sensor_logs.append(CANLog(timestamp=message.data['Timestamp'][i],
+                                              sensor_type=get_sensor_id(measurement),
+                                              value=message.data['Value'][i]))
             
             if len(sensor_logs) > 0:
                 try:
